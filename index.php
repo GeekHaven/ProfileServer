f1a20d1112b2696ea86bb9b7752120e8dc7ed894
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-16 18:13:52 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile Server</title>
+	<link rel="stylesheet" type="text/css" href="assets/stylesheet/index.css">
+	<script type="text/javascript" src="assets/javascript/index.js"></script>
+</head>
+<body>
+
+</body>
+</html>
