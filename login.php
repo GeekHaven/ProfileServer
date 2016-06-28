abd3ae993bbb3984a83875ac628582d243fc64a8
Chaitanya Yadav <cayadav2009@gmail.com>
2016-06-28 07:32:09 +0530

    Add Login Page

---
+++ login.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile Server | IIITA</title>
+	<!-- Stylesheets -->
+	<!--Import materialize.css-->
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<link type="text/css" rel="stylesheet" href="assets/stylesheet/materialize.min.css"  media="screen,projection"/>
+	<link rel="stylesheet" type="text/css" href="assets/stylesheet/login.css">
+
+	<!-- JavaScripts -->
+    <script type="text/javascript" src="assets/javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/javascript/materialize.min.js"></script>
+	  <script type="text/javascript" src="assets/javascript/login.js"></script>
+
+	<!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    
+    
+
+</head>
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">ProServer</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="http://www.iiita.ac.in">IIITA</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="http://iiita.ac.in">IIITA</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center  text-lighten-2">Profile Server IIITA</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">A Modern Server for Keeping Track of Student Profile and Attendance of IIITA Campus</h5>
+          <h6 class="header col s12 light">It is Network of Student and Faculty of IIITA</h6>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="images/bg1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <div class="col s4 offset-s4">
+            <div class="row">
+              <h5>Login With LDAP</h5>
+                <form class="col s12">
+                   <div class="row">
+                      <div class="input-field col s12">
+                        <input id="email" type="email" class="validate">
+                          <label for="email">Email</label>
+                      </div>
+                  </div>
+                    <div class="row">
+                      <div class="input-field col s12">
+                        <input id="password" type="password" class="validate">
+                        <label for="password">Password</label>
+                      </div>
+                  </div>
+                  <div class="row">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                      <i class="material-icons right">send</i>
+                    </button>
+                  </div>
+                </form>
+              </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light"></h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="images/bg2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+            <h5 class="center">Faculty</h5>
+
+            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+            <h5 class="center">Students</h5>
+
+            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Everything At One Place</h5>
+
+            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light"></h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="images/bg3.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+  </body>
+</html>
+
+</html>
