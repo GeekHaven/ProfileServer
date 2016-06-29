be7d602d9e35ab9195c49d9521fe1166aa472cdb
shubham-padia <shubhamapadia@gmail.com>
2016-06-29 22:31:03 +0530

    pages for login system added

--- login.php
+++ login.php
@@ -1,146 +1,77 @@
-<!DOCTYPE html>
-<html>
+<?php
+session_start();
+include_once 'php_includes/dbconnect.php';
+include 'php_includes/logged_in_redirect.php';
+
+
+if(isset($_POST['btn-login']))
+{
+	$email = mysql_real_escape_string($_POST['email']);
+	$pass = mysql_real_escape_string($_POST['pass']);
+	
+	$email = trim($email);
+	$pass = trim($pass);
+
+	
+	$res=mysql_query("SELECT uid, uname, pass FROM profileserver WHERE email='$email'");
+	$row=mysql_fetch_array($res);
+	
+	$count = mysql_num_rows($res); // if uname/pass correct it must return row
+	
+	if($count == 1 && $row['pass']==md5($pass))
+	{
+		$_SESSION['user'] = $row['uid'];
+		header("Location: form.php");
+	}
+	else
+	{
+		?>
+        <script>alert('Username / Password Seems Wrong !');</script>
+        <?php
+	}
+	
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Profile Server | IIITA</title>
-	<!-- Stylesheets -->
-	<!--Import materialize.css-->
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-	<link type="text/css" rel="stylesheet" href="assets/stylesheet/materialize.min.css"  media="screen,projection"/>
-	<link rel="stylesheet" type="text/css" href="assets/stylesheet/login.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>profileserver</title>
 
-	<!-- JavaScripts -->
-    <script type="text/javascript" src="assets/javascript/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/javascript/materialize.min.js"></script>
-	  <script type="text/javascript" src="assets/javascript/login.js"></script>
-
-	<!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
-    
-
+<link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 <body>
-  <nav class="white" role="navigation">
-    <div class="nav-wrapper container">
-      <a id="logo-container" href="#" class="brand-logo">ProServer</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="http://www.iiita.ac.in">IIITA</a></li>
-      </ul>
 
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="http://iiita.ac.in">IIITA</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
+<nav>
+    <ul class="nav">
+        <li>
+          
+        </li></ul></nav>
+<center>
+<div id="login-form" class="center">
+<form method="post" >
+<table class="center "  border="0">
+<tr>
+<td><input type="text" name="email" placeholder="Your Email" required /></td>
+</tr>
+<tr>
+<td><input type="password" name="pass" placeholder="Your Password" required /></td>
+</tr>
+<tr>
+<td><button type="submit" name="btn-login">Sign In</button></td>
+</tr>
+<tr>
+<td><button type="button" name="signup" onclick ="window.location.assign('register.php')">Create a New Account</button></td>
+</tr>
 
-  <div id="index-banner" class="parallax-container">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <br><br>
-        <h1 class="header center  text-lighten-2">Profile Server IIITA</h1>
-        <div class="row center">
-          <h5 class="header col s12 light">A Modern Server for Keeping Track of Student Profile and Attendance of IIITA Campus</h5>
-          <h6 class="header col s12 light">It is Network of Student and Faculty of IIITA</h6>
-        </div>
-        <br><br>
+</tr>
+</table>
 
-      </div>
-    </div>
-    <div class="parallax"><img src="images/bg1.jpg" alt="Unsplashed background img 1"></div>
-  </div>
-  <div class="container">
-    <div class="section">
-
-      <div class="row">
-        <div class="col s12 center">
-          <div class="col s4 offset-s4">
-            <div class="row">
-              <h5>Login With LDAP</h5>
-                <form class="col s12">
-                   <div class="row">
-                      <div class="input-field col s12">
-                        <input id="email" type="email" class="validate">
-                          <label for="email">Email</label>
-                      </div>
-                  </div>
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input id="password" type="password" class="validate">
-                        <label for="password">Password</label>
-                      </div>
-                  </div>
-                  <div class="row">
-                    <button class="btn waves-effect waves-light" type="submit" name="action">Login
-                      <i class="material-icons right">send</i>
-                    </button>
-                  </div>
-                </form>
-              </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-  <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light"></h5>
-        </div>
-      </div>
-    </div>
-    <div class="parallax"><img src="images/bg2.jpg" alt="Unsplashed background img 2"></div>
-  </div>
-
-  <div class="container">
-    <div class="section">
-
-      <!--   Icon Section   -->
-      <div class="row">
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-            <h5 class="center">Faculty</h5>
-
-            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
-          </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-            <h5 class="center">Students</h5>
-
-            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
-          </div>
-        </div>
-
-        <div class="col s12 m4">
-          <div class="icon-block">
-            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-            <h5 class="center">Everything At One Place</h5>
-
-            <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse diam neque, molestie nec hendrerit a, tempus et nibh. Vestibulum dolor mauris, luctus vel laoreet id, feugiat a metus. Duis tincidunt sit amet nunc id accumsan. Vestibulum eget mi scelerisque, pellentesque orci sit amet, pretium arcu. Curabitur sem purus, congue at sagittis ac, pharetra id odio. Mauris viverra ipsum quis arcu tristique, et bibendum turpis porta.</p>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </div>
-  <div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-      <div class="container">
-        <div class="row center">
-          <h5 class="header col s12 light"></h5>
-        </div>
-      </div>
-    </div>
-    <div class="parallax"><img src="images/bg3.jpg" alt="Unsplashed background img 3"></div>
-  </div>
-  </body>
-</html>
-
+</form>
+	
+</div>
+</center>
+</div>
+</body>
 </html>
