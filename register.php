cf9ce65d62d8315b3d4ece26f9304ab49d212083
shubham-padia <shubhamapadia@gmail.com>
2016-07-02 00:08:58 +0530

    fixed some indentation mistakes

--- register.php
+++ register.php
@@ -7,12 +7,10 @@ if(isset($_POST['btn-signup']))
 {
 	$uname = mysql_real_escape_string($_POST['uname']);
 	$email = mysql_real_escape_string($_POST['email']);
-	$pass = md5(mysql_real_escape_string($_POST['pass']));
+	$pass  = md5(mysql_real_escape_string($_POST['pass']));
 	$fname = mysql_real_escape_string($_POST['first_name']);
 	$lname = mysql_real_escape_string($_POST['last_name']);
 	$mname = mysql_real_escape_string($_POST['middle_name']);
-
-
 
 	$uname = trim($uname);
 	$email = trim($email);
@@ -21,11 +19,9 @@ if(isset($_POST['btn-signup']))
 	$lname = trim($lname);
 	$mname = trim($mname);
 
-
 	// email exist or not
 	$query = "SELECT email FROM profileserver WHERE email='$email'";
-	$result = mysql_query($query);
-	
+	$result = mysql_query($query);	
 	$count = mysql_num_rows($result); // if email not found then register
 	
 	if($count == 0){
@@ -49,59 +45,53 @@ if(isset($_POST['btn-signup']))
 			<script>alert('Sorry Email ID already taken ...');</script>
 			<?php
 	}
-	
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!DOCTYPE html >
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Register</title>
-<link rel="stylesheet" href="css/temp_login.css" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Register</title>
+	<link rel="stylesheet" href="css/temp_login.css" type="text/css" />
 </head>
+
 <body>
 <center>
-<div id = "snow">
-<nav>
-    <ul class="nav">
-        <li>
-          <a href="index.php">
-            <span class="icon-home"></span>
-            <span class="screen-reader-text">Home</span>
-          </a>
-        </li></ul></nav>
 <div id="login-form">
-<form method="post">
-<table width="60%" border="0">
-<tr>
-<td><input type="text" name="first_name" placeholder="First Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="middle_name" placeholder="Middle Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="last_name" placeholder="Last Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="uname" placeholder="User Name" required /></td>
-</tr>
-<tr>
-<td><input type="email" name="email" placeholder="Your Email" required /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-signup">Register</button></td>
-</tr>
-<tr>
-<td><button type="button" name="button" onclick="window.location.assign('login.php')">Already Registered? Login here</button></td>
 
-</tr>
-</table>
-</form>
+	<form method="post">
+	<table width="60%" border="0">
+		<tr>
+			<td><input type="text" name="first_name" placeholder="First Name" required /></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="middle_name" placeholder="Middle Name" required /></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="last_name" placeholder="Last Name" required /></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="uname" placeholder="User Name" required /></td>
+		</tr>
+		<tr>
+			<td><input type="email" name="email" placeholder="Your Email" required /></td>
+		</tr>
+		<tr>
+			<td><input type="password" name="pass" placeholder="Your Password" required /></td>
+		</tr>
+		<tr>
+			<td><button type="submit" name="btn-signup">Register</button></td>
+		</tr>
+		<tr>
+			<td><button type="button" name="button" onclick="window.location.assign('login.php')">Already Registered? Login here</button></td>
+		</tr>
+	</table>
+	</form>
+
 </div>
-</center></div>
+</center>
+</div>
 </body>
+
 </html>
 
