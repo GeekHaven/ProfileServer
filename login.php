eaf1de92d1ad3a659125c7ff12430fc8f9b18311
neha <kandpalnehu13@gmail.com>
2016-07-05 03:39:06 +0530

    login and register pages changed

--- login.php
+++ login.php
@@ -24,46 +24,72 @@ if(isset($_POST['btn-login']))
 		}
 }		
 ?>
-
+ 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>profileserver</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>profileserver</title>
+  <!-- Stylesheets -->
+  <!--Import materialize.css-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <link rel="stylesheet" type="text/css" href="css/temp_login.css">
 
-<link rel="stylesheet" href="css/temp_login.css" type="text/css" />
+  <!-- JavaScripts -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script type="text/javascript" src="js/ldap_login.js"></script>
+
+  <!--Import Google Icon Font-->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
 
-<nav>
-    <ul class="nav">
-        <li>
-          
-        </li></ul></nav>
-<center>
-<div id="login-form" class="center">
-<form method="post" >
-<table class="center "  border="0">
-<tr>
-<td><input type="text" name="email" placeholder="Your Email" required /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-login">Sign In</button></td>
-</tr>
-<tr>
-<td><button type="button" name="signup" onclick ="window.location.assign('register.php')">Create a New Account</button></td>
-</tr>
-
-</tr>
-</table>
-
-</form>
-	
-</div>
-</center>
-</div>
+  <nav>
+      <div class="nav-wrapper  cyan darken-4"  id="nav-wrapper">
+        <a href="#" class="brand-logo" id="header-logo">
+          <img src="images/effe.png">
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
+          <li class="active"><a href="#" id="header-user" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Minion</a></li>
+          <li><a href="#" id="header-login" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Login</b></a></li>
+          <li><a href="#" id="header-signup" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Sign Up</a></li>
+          <li><a href="#" id="header-settings" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Something</a></li>
+        </ul>
+      </div>
+  </nav>
+<div id="head" >
+    <div class="section no-pad-bot">
+      <h1><center> Sign In </center> </h1>
+      <div class="container" style="width:500px;">
+        <div id="login-form" class="center">
+          <form method="post" >
+            <center>
+            <table class="center" style="border-bottom:solid #006064 70px;  width:80%; height=30%; backgroung:#b0bec5; border-collapse:separate; border-spacing: 5px;">
+              <tr bgcolor="#9e9e9e">
+                <td><i class="material-icons prefix">perm_identity</i><input type="text" name="email" placeholder="Your Email" required /></td>
+              </tr>
+              <tr bgcolor="#9e9e9e">
+                <td><i class="material-icons prefix">mode_edit</i><input type="password" name="pass" placeholder="Your Password" required /></td>
+              </tr bgcolor="#9e9e9e">
+              <div class="container" style="backgroung:#006064">
+              <tr>
+                <td><button type="submit" name="btn-login">Sign In</button></td>
+              </tr>
+              <tr>
+                <td><button type="button" name="signup" onclick ="window.location.assign('register.php')">Create a New Account</button></td>
+              </tr>
+              </div>
+            </table>
+          </center>
+          </form>
+        </div>
+      </div>
+      <br><br>
+    </div>
+  </div>
 </body>
 </html>
+
--- register.php
+++ register.php
@@ -69,54 +69,73 @@ if(isset($_POST['btn-signup']))
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Register</title>
-<link rel="stylesheet" href="css/temp_login.css" type="text/css" />
-</head>
-<body>
-<center>
-<div id = "snow">
-<nav>
-    <ul class="nav">
-        <li>
-          <a href="index.php">
-            <span class="icon-home"></span>
-            <span class="screen-reader-text">Home</span>
-          </a>
-        </li></ul></nav>
-<div id="login-form">
-<form method="post">
-<table width="60%" border="0">
-<tr>
-<td><input type="text" name="first_name" placeholder="First Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="middle_name" placeholder="Middle Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="last_name" placeholder="Last Name" required /></td>
-</tr>
-<tr>
-<td><input type="text" name="uname" placeholder="User Name" required /></td>
-</tr>
-<tr>
-<td><input type="email" name="email" placeholder="Your Email" required /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-signup">Register</button></td>
-</tr>
-<tr>
-<td><button type="button" name="button" onclick="window.location.assign('login.php')">Already Registered? Login here</button></td>
-
-</tr>
-</table>
-</form>
-</div>
-</center></div>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Register</title>
+    <!-- Stylesheets -->
+    <!--Import materialize.css-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" type="text/css" href="css/temp_login.css">
+    <!-- JavaScripts -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/ldap_login.js"></script>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+  <body>
+    <nav>
+      <div class="nav-wrapper  cyan darken-4"  id="nav-wrapper">
+        <a href="#" class="brand-logo" id="header-logo">
+          <img src="images/effe.png">
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
+          <li class="active"><a href="#" id="header-user" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Minion</a></li>
+          <li><a href="#" id="header-login" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Login</b></a></li>
+          <li><a href="#" id="header-signup" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Sign Up</a></li>
+          <li><a href="#" id="header-settings" style="color:white; font-size:20px; font-family:Comic Sans MS;"><b>>Something</a></li>
+        </ul>
+      </div>
+    </nav>
+    <center>
+      <div id = "snow">
+        <h1> Login here </h1>
+        <div class="container" id="login-form">
+          <form method="post">
+            <table width="100%" border="0">
+              <div class="row">
+              <tr>
+                <td><i class="material-icons prefix">account_circle</i><input type="text" name="first_name" placeholder="FirstName" required /></td>
+                <td><i class="material-icons prefix">account_circle</i><input type="text" name="first_name" placeholder="LastName" required /></td>
+              </tr>
+              <tr>
+                <td><i class="material-icons prefix">perm_identity</i><input type="text" name="uname" placeholder="User Name" required /></td>
+                <td><i class="material-icons prefix">mode_edit</i><input type="password" name="pass" placeholder="Your Password" required /></td>
+              </tr>
+                <td><i class="material-icons prefix">explicit</i><input type="text" name="uname" placeholder="Your email" required /></td>
+                <td><i class="material-icons prefix">phone</i><input type="email" name="email" placeholder="Contact No." required /></td>
+              </tr>
+              <tr>
+                
+              </tr>
+              <tr>
+                <td colspan='2'><center> <button type="submit" name="btn-signup">Register</button></center></td>
+              </tr>
+              <tr>
+                <td colspan='2'><button type="button" name="button" onclick="window.location.assign('login.php')">Already Registered? Login here</button></td>
+              </tr>
+            </div>
+            </table>
+          </form>
+        </div>
+      </center>
+      <div class="parallax"><img src="images/bg5.jpg" alt="Unsplashed background img 1" height=200%; width=100%;></div>
+    </div>
+  <br><br>
 </body>
 </html>
+
+
 
