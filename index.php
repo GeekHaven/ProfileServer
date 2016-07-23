fb91bb6c7d23991bb31696cdb20e4e671873f886
shubham-padia <shubhamapadia@gmail.com>
2016-07-23 16:24:22 +0530

    Set up url routing
    /profileserver/rollno will be equivalent to /profileserver/user.php?roll_no=rollno

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$request  = str_replace("/profileserver/", "", $_SERVER['REQUEST_URI']);
+$params = split("/", $request);
+
+$pages = array("login", "register", "attendance");
+
+if(in_array($params[0], $pages)) {
+	include($params[0].".php");
+} else if($params[0]=="edit") {
+	include "form.php";
+} else if($params[0]=="404") {
+	include "php_includes/404.php";
+} else {
+	$_GET['roll_no'] = $params[0];
+	include 'user.php';
+}
+?>
